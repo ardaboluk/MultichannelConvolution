b352ed0ef0fad502902db451533cc40d71d7ba5b
ardaboluk <ardaboluk2@gmail.com>
2019-09-28 15:02:54 +0300

    Made the test code a function. Added OpenCV test code as another function.

--- ConvolutionTest/ConvolutionTest.cpp
+++ ConvolutionTest/ConvolutionTest.cpp
@@ -16,56 +16,29 @@ using namespace std;
 string imageFileName;
 string kernelFileName;
 string resultImageFileName;
+string resultImgeFileNameOpenCV;
 
 void getFileNamesFromUser();
+void testCustomModule();
+void testOpenCV();
 
 int main(int argc, char *argv[])
-{
-	try {
-		
-		if (argc > 2) {
-			imageFileName = argv[1];
-			kernelFileName = argv[2];
-		}
-		else {
-			getFileNamesFromUser();
-		}
-		char delim[1] = { '.' };
-		resultImageFileName = imageFileName.substr(0, imageFileName.find("."));
-		resultImageFileName += "_filtered.jpg";
-
-		cv::Mat imageData = cv::imread(imageFileName);
-		Image3* image = new Image3(&imageData);
-		Kernel3* kernel = Util::readKernel3FromFile(kernelFileName);
-
-		Convolution3 conv3(image, kernel);
-
-		Point p1(0, 0);
-		Point p2(image->getWidth() - 1, image->getHeight() - 1);
-		Rectangle wholeImageRegion(p1, p2);
-
-		clock_t begin = clock();
-		Image3* resultImage = conv3.convolute(wholeImageRegion);
-		clock_t end = clock();
-		cout << "Elapsed time in convolution: " << endl;
-		cout << double(end - begin) / CLOCKS_PER_SEC;
-
-		cv::imwrite(resultImageFileName, *(resultImage->getDataWrapper()->getData()));
-		cv::namedWindow("Filtered Image");
-		cv::imshow("Filtered Image", *(resultImage->getDataWrapper()->getData()));
-		cv::waitKey(0);
-
-		imageData.release();
-		kernel->getDataWrapper()->getData()->release();
-		resultImage->getDataWrapper()->getData()->release();
-
-		delete image;
-		delete kernel;
-		delete resultImage;
+{		
+	if (argc > 2) {
+		imageFileName = argv[1];
+		kernelFileName = argv[2];
 	}
-	catch (const exception& e) {
-		cout<<e.what();
+	else {
+		getFileNamesFromUser();
 	}
+	char delim[1] = { '.' };
+	resultImageFileName = imageFileName.substr(0, imageFileName.find("."));
+	resultImgeFileNameOpenCV = string(resultImageFileName);
+	resultImageFileName += "_filtered.jpg";
+	resultImgeFileNameOpenCV += "_filtered_opencv.jpg";
+
+	testCustomModule();
+	testOpenCV();
 
 	return 0;
 }
@@ -87,4 +60,86 @@ void getFileNamesFromUser() {
 		cin >> tmp;
 	}
 	kernelFileName = tmp;
+}
+
+void testCustomModule() {
+	try {
+		cv::Mat imageData = cv::imread(imageFileName);
+		Image3* image = new Image3(&imageData);
+		Kernel3* kernel = Util::readKernel3FromFile(kernelFileName);
+
+		Convolution3 conv3(image, kernel);
+
+		Point p1(0, 0);
+		Point p2(image->getWidth() - 1, image->getHeight() - 1);
+		Rectangle wholeImageRegion(p1, p2);
+
+		clock_t begin = clock();
+		Image3* resultImage = conv3.convolute(wholeImageRegion);
+		clock_t end = clock();
+		cout << "Elapsed time in convolution: " << endl;
+		cout << double(end - begin) / CLOCKS_PER_SEC << endl;
+
+		cv::imwrite(resultImageFileName, *(resultImage->getDataWrapper()->getData()));
+		cv::namedWindow("Filtered Image");
+		cv::imshow("Filtered Image", *(resultImage->getDataWrapper()->getData()));
+		cv::waitKey(0);
+
+		imageData.release();
+		kernel->getDataWrapper()->getData()->release();
+		resultImage->getDataWrapper()->getData()->release();
+
+		delete image;
+		delete kernel;
+		delete resultImage;
+	}
+	catch (const exception& e) {
+		cout << e.what();
+	}
+}
+
+void testOpenCV() {
+
+	cv::Point anchor(-1, -1);
+	double delta = 0;
+	int borderType = cv::BORDER_REFLECT;
+
+	cv::Mat imgOriginal = cv::imread(imageFileName);
+	cv::Mat imgFilteredSplit[3] = { {imgOriginal.size(), CV_8UC1}, {imgOriginal.size(), CV_8UC1}, {imgOriginal.size(), CV_8UC1} };
+	cv::Mat bgrChannel[3];
+	cv::Mat imgFiltered;
+	cv::split(imgOriginal, bgrChannel);
+
+	cv::Mat kernelB(3, 3, CV_32FC1), kernelG(3, 3, CV_32FC1), kernelR(3, 3, CV_32FC1);
+	Kernel3* kernel = Util::readKernel3FromFile(kernelFileName);
+	cv::Size kernelSize = kernel->getDataWrapper()->getData()->size();
+	cv::Mat kernelChannel[3] = { {kernelSize, CV_8UC1}, {kernelSize, CV_8UC1}, {kernelSize, CV_8UC1} };;
+	cv::split(*kernel->getDataWrapper()->getData(), kernelChannel);
+	kernelB = kernelChannel[0];
+	kernelG = kernelChannel[1];
+	kernelR = kernelChannel[2];
+
+	clock_t begin = clock();
+	filter2D(bgrChannel[0], imgFilteredSplit[0], -1, kernelB, anchor, delta, borderType);
+	filter2D(bgrChannel[1], imgFilteredSplit[1], -1, kernelG, anchor, delta, borderType);
+	filter2D(bgrChannel[2], imgFilteredSplit[2], -1, kernelR, anchor, delta, borderType);
+	clock_t end = clock();
+	cout << "Elapsed time in convolution in OpenCV: " << endl;
+	cout << double(end - begin) / CLOCKS_PER_SEC << endl;
+
+	cv::merge(&(imgFilteredSplit[0]), 3, imgFiltered);
+
+	cv::namedWindow("Filtered Image OpenCV", cv::WINDOW_NORMAL);
+	imshow("Filtered Image OpenCV", imgFiltered);
+	imwrite(resultImgeFileNameOpenCV, imgFiltered);
+	cv::waitKey(0);
+
+	imgOriginal.release();
+	kernel->getDataWrapper()->getData()->release();
+	imgFiltered.release();
+	kernelB.release();
+	kernelG.release();
+	kernelR.release();
+
+	delete kernel;
 }
